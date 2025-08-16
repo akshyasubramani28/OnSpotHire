d6dfe57b88370e4eff6aaf145f5a85a6bbb68315
akshyasubramani28 <akshyasubramani597@gmail.com>
2025-08-16 16:27:33 +0530

    Initial commit - OnSpotHiring project setup

---
+++ src/main/java/org/example/servlet/HomeServlet.java
@@ -0,0 +1,18 @@
+package org.example.servlet;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Hello from HomeServlet!</h1>");
+    }
+}
